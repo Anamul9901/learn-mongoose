6412fea438627c0453763254208542a9f49cc202
Anamul9901 <anamulhaque9901@gmail.com>
2024-04-14 18:32:20 +0600

    todo put api

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -33,20 +33,33 @@ router.post("/", async (req, res) => {
 });
 
 // POST multipal todo
-router.post("/all", async (req, res)=>{
-  try{
+router.post("/all", async (req, res) => {
+  try {
     await Todo.insertMany(req.body);
-    res.status(201).json({message: "Todo were created successfully"})
-  }catch(err){
+    res.status(201).json({ message: "Todo were created successfully" });
+  } catch (err) {
     console.error(err);
-    res.status(500).json({error: "Server error"})
+    res.status(500).json({ error: "Server error" });
   }
-})
+});
 
+// PUT Todo
+router.put("/:id", async (req, res) => {
+  try {
+    const result = await Todo.updateOne(
+      { _id: req.params.id },
+      { $set: { status: "inactive" } }
+    );
+    if (result.nModified === 0) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+    res.status(200).json({ MESSAGE: "Todo was updated successfully!" });
+  } catch (err) {
+    console.error("Error updating Todo:", err);
+    res.status(500).json({ error: "There was a server side error!" });
+  }
+});
 
-
-//PUT Todo
-router.put("/:id", async (req, res) => {});
 
 //Delete todo
 router.delete("/:id", async (req, res) => {});
